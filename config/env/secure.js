be1cfb49cb5b5a4fc6f209ce9d0b91b14d25970b
nancy <nancy@nancys-MacBook-Air.local>
2016-02-19 16:16:00 -0700

    modified

--- config/env/secure.js
+++ config/env/secure.js
@@ -2,7 +2,7 @@
 
 module.exports = {
 	port: 443,
-	db: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://localhost/northwindnode',
+	db: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://localhost/simpledb',
 	assets: {
 		lib: {
 			css: [
