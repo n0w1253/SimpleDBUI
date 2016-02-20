f73e4562777dc18405cd2d769359c63e9570fd6b
nancy <nancy@nancys-MacBook-Air.local>
2016-02-19 17:34:30 -0700

    modified gui

--- public/modules/categories/controllers/categories.client.controller.js
+++ public/modules/categories/controllers/categories.client.controller.js
@@ -2,79 +2,85 @@
 
 // Categories controller
 angular.module('categories').controller('CategoriesController', ['$scope', '$stateParams', '$location', 'Authentication', 'Categories',
-	function($scope, $stateParams, $location, Authentication, Categories) {
-		$scope.authentication = Authentication;
-	  	$scope.currentPage = 1;
-	  	$scope.pageSize = 10;
-	  	$scope.offset = 0;
+    function ($scope, $stateParams, $location, Authentication, Categories) {
+        $scope.authentication = Authentication;
+        $scope.currentPage = 1;
+        $scope.pageSize = 10;
+        $scope.offset = 0;
 
-	  	// Page changed handler
-	  	$scope.pageChanged = function() {
-	   	$scope.offset = ($scope.currentPage - 1) * $scope.pageSize;
-	  	};
+        $scope.sortType = 'name'; // set the default sort type
+        $scope.sortReverse = false;  // set the default sort order
+        $scope.searchTerm = '';     // set the default search/filter term
 
-		// Create new Category
-		$scope.create = function() {
-			// Create new Category object
-			var category = new Categories ({
-				name: this.name,
-				description: this.description
-			});
+        // Page changed handler
+        $scope.pageChanged = function () {
+            $scope.offset = ($scope.currentPage - 1) * $scope.pageSize;
+        };
 
-			// Redirect after save
-			category.$save(function(response) {
-				$location.path('categories/' + response._id);
+        // Create new Category
+        $scope.create = function () {
+            // Create new Category object
+            var category = new Categories({
+                name: this.name,
+                description: this.description
+            });
 
-				// Clear form fields
-				$scope.name = '';
-			}, function(errorResponse) {
-				$scope.error = errorResponse.data.message;
-			});
-		};
+            // Redirect after save
+            category.$save(function (response) {
+                //$location.path('categories/' + response._id);
+                $location.path('categories');
 
-		// Remove existing Category
-		$scope.remove = function(category) {
-			if ( category ) { 
-				category.$remove();
+                // Clear form fields
+                $scope.name = '';
+            }, function (errorResponse) {
+                $scope.error = errorResponse.data.message;
+            });
+        };
 
-				for (var i in $scope.categories) {
-					if ($scope.categories [i] === category) {
-						$scope.categories.splice(i, 1);
-					}
-				}
-			} else {
-				$scope.category.$remove(function() {
-					$location.path('categories');
-				});
-			}
-		};
+        // Remove existing Category
+        $scope.remove = function (category) {
+            if (category) {
+                category.$remove();
 
-		// Update existing Category
-		$scope.update = function() {
-			var category = $scope.category;
+                for (var i in $scope.categories) {
+                    if ($scope.categories [i] === category) {
+                        $scope.categories.splice(i, 1);
+                    }
+                }
+            } else {
+                $scope.category.$remove(function () {
+                    $location.path('categories');
+                });
+            }
+        };
 
-			category.$update(function() {
-				$location.path('categories/' + category._id);
-			}, function(errorResponse) {
-				$scope.error = errorResponse.data.message;
-			});
-		};
+        // Update existing Category
+        $scope.update = function () {
+            var category = $scope.category;
 
-		// Find a list of Categories
-		$scope.find = function() {
-			$scope.categories = Categories.query();
-		};
+            category.$update(function () {
+                //$location.path('categories/' + category._id);
+                $location.path('categories');
+            }, function (errorResponse) {
+                $scope.error = errorResponse.data.message;
+            });
+        };
 
-		// Find existing Category
-		$scope.findOne = function() {
-			$scope.category = Categories.get({ 
-				categoryId: $stateParams.categoryId
-			});
-		};
+        // Find a list of Categories
+        $scope.find = function () {
+            $scope.categories = Categories.query();
+        };
 
-		// Search for a category
-		$scope.categorySearch = function(product) {
-			$location.path('categories/' + product._id);
-		};
-	}
+        // Find existing Category
+        $scope.findOne = function () {
+            $scope.category = Categories.get({
+                categoryId: $stateParams.categoryId
+            });
+        };
+
+        // Search for a category
+        $scope.categorySearch = function (product) {
+            $location.path('categories/' + product._id);
+        };
+    }
 ]);
